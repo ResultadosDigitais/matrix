464a323277c9b48aa6cef974dd6c9ed70b10176e
juliemar.berri <juliemar.berri@resultadosdigitais.com.br>
2019-03-27 20:02:54 -0300

    profile test

---
+++ test/public/profile.test.js
@@ -0,0 +1,29 @@
+require('mock-local-storage') 
+const assert = require('assert');
+const expect = require('chai').expect
+const request = require('supertest');
+var MatrixProfile =  require('../../public/profile');
+
+
+
+describe('Basic profile credential Test', function () {
+	it('should return empty of credentials is not present', function () {
+
+		var matrixProfile = new MatrixProfile();
+        assert.equal(matrixProfile.loadStoredProfile(), null);
+    });
+
+    it('should return data of credentials is present', function () {
+
+    	var profileData = {
+    		id: '111',
+    		name: 'Nome do fulano',
+    		imageUrl: 'http://localhost/img.jpg',
+    		email: 'Mail@mail.com' 
+    	};
+
+		var matrixProfile = new MatrixProfile();
+		matrixProfile.storeProfileData(profileData);
+        assert.equal(matrixProfile.loadStoredProfile().name, profileData.name);
+    });
+});
