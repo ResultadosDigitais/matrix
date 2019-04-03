e22b7a1e9d5911feb03bda40ef11bd650e393966
Jorge Modesto <jorgemodes@gmail.com>
2019-04-03 12:30:49 -0300

    Add bootstrap to server and index

--- app.server.js
+++ app.server.js
@@ -1,26 +1,27 @@
 const express = require('express')
-const GoogleCredentialController =  require('./controllers/google.credentials.controller');
+const GoogleCredentialController = require('./controllers/google.credentials.controller');
 const app = express()
 const Office = require('./office.server')
 const fs = require('fs');
 
 const PORT = process.env.PORT || 8080
 const HOST = '0.0.0.0';
-const GOOGLECREDENTIAL = process.env.googleCredential || "990846956506-bfhbjsu4nl5mvlkngr3tsmfcek24e8t8.apps.googleusercontent.com" 
+const GOOGLECREDENTIAL = process.env.googleCredential || "990846956506-bfhbjsu4nl5mvlkngr3tsmfcek24e8t8.apps.googleusercontent.com"
 
 //set the template engine ejs
 app.set('view engine', 'ejs')
 
 //middlewares
 app.use(express.static('public'))
+app.use('/vendor/css/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 //FIX ME: here we have to get the google APIkey in another way.
 app.locals.googleCredential = new GoogleCredentialController(GOOGLECREDENTIAL)
 
 
-let roomData = process.env.roomData || fs.readFileSync('./file/default.room.web.json');  
-let roomsDetail = JSON.parse(roomData);  
-console.log(roomsDetail); 
+let roomData = process.env.roomData || fs.readFileSync('./file/default.room.web.json');
+let roomsDetail = JSON.parse(roomData);
+console.log(roomsDetail);
 
 app.locals.roomsDetail = roomsDetail;
 
@@ -37,6 +38,6 @@ app.get('/office', (req, res) => {
 server = app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
-new Office(server) 
+new Office(server)
 
 module.exports = server
