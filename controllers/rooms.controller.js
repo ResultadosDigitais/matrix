2e5c9b4f4cbde421436c4341776aaacb8cae0da5
= <v@fractopia.org>
2019-05-09 20:10:50 -0300

    Allways use env vars for rooms

--- controllers/rooms.controller.js
+++ controllers/rooms.controller.js
@@ -1,11 +1,11 @@
 import fs from "fs";
 
-const fetchFromFile = () => {
-  const roomsData = fs.readFileSync("./file/default.room.web.json");
-  const roomsDetail = JSON.parse(roomsData);
-
-  return new Promise((resolve, reject) => resolve(roomsDetail));
-};
+// const fetchFromFile = () => {
+//   const roomsData = fs.readFileSync("./file/default.room.web.json");
+//   const roomsDetail = JSON.parse(roomsData);
+//
+//   return new Promise((resolve, reject) => resolve(roomsDetail));
+// };
 
 const fetchFromEnvironment = env => {
   const roomsData = env.ROOMS_DATA;
@@ -15,13 +15,16 @@ const fetchFromEnvironment = env => {
 };
 
 const fetchRooms = strategy => {
-  switch (strategy) {
+  return fetchFromEnvironment(process.env);
+
+  // switch (strategy) {
     //TODO add suport to fetch from endpoint
-    case "ENVIRONMENT":
-      return fetchFromEnvironment(process.env);
-    default:
-      return fetchFromFile();
-  }
+    // case "ENVIRONMENT":
+    //   return fetchFromEnvironment(process.env);
+    // default:
+    //   return fetchFromFile();
+    // default:
+  // }
 };
 
 export default fetchRooms;
