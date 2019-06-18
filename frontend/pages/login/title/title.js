9e08adb0edeba88b96aab23c820629ebba3ead34
Alessandro Tegner <alessandro.tegner@gmail.com>
2019-06-18 19:15:13 -0300

    React at login (#163)
    * Setup react
    
    * Reimplement login page in react
    
    * Extract CSS modules
    
    * Fix broken tests

---
+++ frontend/pages/login/title/title.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Title = () => (
+  <div className="row justify-content-center align-items-center p-3">
+    <h5 className="text-center">Work everywhere. Stay Together</h5>
+  </div>
+);
