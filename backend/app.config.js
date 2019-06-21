76b3946a43d389663666169d66ceaaf7566500b8
Bruno Nardini <bruno.nardini@m4u.com.br>
2019-06-21 16:37:52 -0300

    Change HOST and PORT to config file

--- backend/app.config.js
+++ backend/app.config.js
@@ -6,4 +6,6 @@ module.exports = {
     "990846956506-bfhbjsu4nl5mvlkngr3tsmfcek24e8t8.apps.googleusercontent.com",
   ENFORCE_SSL: process.env.ENFORCE_SSL || "false",
   CUSTOM_STYLE: process.env.CUSTOM_STYLE || "",
+  HOST: "0.0.0.0",
+  PORT: process.env.PORT || 8080,
 };
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 import app from "./app.server";
 import officeFactory from "./office.factory";
-
-const PORT = process.env.PORT || 8080;
-const HOST = "0.0.0.0";
+import { HOST, PORT } from "./app.config";
 
 const server = app.listen(PORT, HOST, undefined, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
