d5fce448e1ecb6e01172f36efb0ddaa9f603b648
Bruno Nardini <bruno.nardini@m4u.com.br>
2019-06-21 15:06:03 -0300

    Create two more tests for enter-room event in socket

--- test/office.socket.test.js
+++ test/office.socket.test.js
@@ -9,14 +9,8 @@ const SOCKET_HOST = "0.0.0.0";
 const SOCKET_PORT = "3003";
 
 describe("Office server", () => {
-  const user = JSON.stringify({ id: 1 });
-  const room = JSON.stringify({});
-  const ioOptions = {
-    transports: ["websocket"],
-    forceNew: true,
-    reconnection: false,
-    query: `user=${user}&room=${room}`,
-  };
+  const defaultUser = JSON.stringify({ id: 1 });
+  let ioOptions;
   let officeController;
   let server;
   let office;
@@ -30,26 +24,37 @@ describe("Office server", () => {
       removeUser: sinon.fake(),
     };
 
-    // start express server
-    server = app.listen(SOCKET_PORT, SOCKET_HOST);
-
-    // start Office server
-    office = new Office(officeController, server);
-
-    // connect a client to Office server
-    socketClient = io.connect(`http://${SOCKET_HOST}:${SOCKET_PORT}/`, ioOptions);
+    ioOptions = {
+      transports: ["websocket"],
+      forceNew: true,
+      reconnection: false,
+      query: `user=${defaultUser}`,
+    };
   });
 
   afterEach(() => {
     if (socketClient) {
       socketClient.disconnect();
     }
-
-    server.close();
+    if (server) {
+      server.close();
+    }
   });
 
-  it("should connect and disconnect with success", (done) => {
+  const connectSocketServer = () => {
+    // start express server
+    server = app.listen(SOCKET_PORT, SOCKET_HOST);
+
+    // start Office server
+    office = new Office(officeController, server);
     office.start();
+
+    // connect a client to Office server
+    socketClient = io.connect(`http://${SOCKET_HOST}:${SOCKET_PORT}/`, ioOptions);
+  };
+
+  it("should connect and disconnect with success", (done) => {
+    connectSocketServer();
 
     socketClient.on("connect", () => {
       assert(officeController.addUserInRoom.calledOnce);
@@ -59,15 +64,47 @@ describe("Office server", () => {
     });
   });
 
+  it("should enter in default room on connect", (done) => {
+    connectSocketServer();
+
+    socketClient.on("connect", () => {
+      socketClient.on("enter-room", () => {
+        assert.equal(
+          officeController.addUserInRoom.getCall(0).lastArg,
+          "room-1",
+        );
+        done();
+      });
+    });
+  });
+
+  it("should enter in query room on connect", (done) => {
+    ioOptions.query = `user=${defaultUser}&room=Nebuchadnezzar`;
+
+    connectSocketServer();
+
+    socketClient.on("connect", () => {
+      socketClient.on("enter-room", () => {
+        assert.equal(
+          officeController.addUserInRoom.getCall(0).lastArg,
+          "Nebuchadnezzar",
+        );
+        done();
+      });
+    });
+  });
+
   it("should enter a room with success", (done) => {
     const payload = {
       room: "test-room",
     };
 
-    office.start();
+    connectSocketServer();
 
     socketClient.on("connect", () => {
       socketClient.on("enter-room", () => {
+        // validates the second event when "enter-room" was emitted
+        // because the first is triggered on connection
         if (officeController.addUserInRoom.callCount === 2) {
           assert.equal(
             officeController.addUserInRoom.getCall(1).lastArg,
