9ded4782a7d6f415908e885ef13c6dc1db0d9542
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2020-03-17 14:37:42 -0300

    Add healthcheck

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,11 +1,13 @@
 import app from "./app.server";
 import officeFactory from "./office.factory";
+import healthCheck from "./app.healthcheck";
 import { HOST, PORT } from "./app.config";
 
 const server = app.listen(PORT, HOST, undefined, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
 
+healthCheck(server);
 officeFactory(server).start();
 
 module.exports = server;
