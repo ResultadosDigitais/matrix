f5f1f99513223a10328d780f12fffefe6d6b3b22
Bruno Nardini <bruno.nardini@resultadosdigitais.com.br>
2020-03-18 17:27:27 -0300

    Add "XXX" to Google ID

--- backend/app/app.config.js
+++ backend/app/app.config.js
@@ -4,11 +4,10 @@ module.exports = {
   ROOMS_SOURCE: process.env.ROOMS_SOURCE,
   ENVIRONMENT: process.env.NODE_ENV,
   SESSION_SECRET: process.env.SESSION_SECRET || "matrix-session",
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || "",
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "XXXX",
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || "XXXX",
   GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL
     || "http://localhost:8080/auth/google/callback",
-  GOOGLE_CREDENTIAL: process.env.GOOGLE_CREDENTIAL || "",
   COOKIE_SESSION_SECRET: process.env.COOKIE_SESSION_SECRET || "",
   COOKIE_SESSION_MAX_AGE: environment.parseVariable(process.env.COOKIE_SESSION_MAX_AGE)
     || 30 * 24 * 60 * 60 * 1000, // 30 days
