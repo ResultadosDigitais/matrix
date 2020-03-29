1e88c73fb3fd55f56393c831acb4ed21c6fb55b5
Alessandro Tegner <alessandro.tegner@gmail.com>
2020-03-29 14:53:34 -0300

    Improve auth service to does not expose passport

--- backend/app/services/auth/index.js
+++ backend/app/services/auth/index.js
@@ -19,40 +19,50 @@ export function setupAppAuth(app) {
   app.use(passport.session());
 }
 
+export function authenticationHandler() {
+  return passport.authenticate(authStrategy, { scope: ["profile", "email"] });
+}
+
+export function authenticationCallbackHandler({
+  successRedirect,
+  failureRedirect
+}) {
+  return function(req, resp, next) {
+    const redirectToFailure = err => {
+      const errorMessage = err || "";
+
+      resp
+        .status(401)
+        .redirect(
+          `${failureRedirect}?error=${encodeURIComponent(errorMessage)}`
+        );
+    };
+
+    passport.authenticate(authStrategy, function(err, profile) {
+      if (err) {
+        return redirectToFailure(err);
+      }
+      if (!profile) {
+        return redirectToFailure();
+      }
+
+      req.login(profile, err => {
+        if (err) {
+          return redirectToFailure(err);
+        }
+
+        resp.redirect(successRedirect);
+      });
+    })(req, resp, next);
+  };
+}
+
 export function currentUser(req) {
   return req.user;
 }
 
 export function isUserLoggedIn(req) {
-  return !!this.currentUser(req);
-}
-
-export function authenticate(opts) {
-  const { loginURL = null } = opts || {};
-
-  return (req, resp, next) => {
-    if (this.isUserLoggedIn(req)) {
-      return next();
-    }
-
-    if (loginURL) {
-      return resp.status(401).redirect(loginURL);
-    }
-
-    resp.sendStatus(401);
-  };
-}
-
-export function login(req, user) {
-  return new Promise((success, reject) => {
-    req.login(user, err => {
-      if (err) {
-        return reject(err);
-      }
-
-      success(user);
-    });
-  });
+  return !!currentUser(req);
 }
 
 export function logout(req) {
