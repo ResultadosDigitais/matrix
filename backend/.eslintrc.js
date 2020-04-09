0fe4bf8a011d6ae06177e7e8444465e9131f8077
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2020-04-09 18:35:12 -0300

    Just remove mocha

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
   env: {
-    node: true
+    node: true,
+    jest: true,
   },
   rules: {
     "no-unused-expressions": ["off"]
-  }
+  },
+  overrides: [
+    {
+      files: ['*.test.js', '*.spec.js'],
+      rules: {
+        "import/no-extraneous-dependencies": "off"
+      },
+    },
+  ],
 };
