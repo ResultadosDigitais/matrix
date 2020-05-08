c223b7252430c88266d754810d84a8cbaf78d605
João Ernesto Arzamendia <jearzamendia@gmail.com>
2020-05-08 19:27:31 -0400

    tune some tests

--- backend/app/services/auth/authorization.test.js
+++ backend/app/services/auth/authorization.test.js
@@ -38,10 +38,10 @@ describe(".domainAuthorization()", () => {
   });
 });
 describe(".userAuthorization()", () => {
-  describe("when user list is empty", () => {
+  describe("when user whitelist is empty or not set", () => {
     const isAuthorized = userAuthorization([]);
 
-    it("should return true with an email", () => {
+    it("should return true with any email", () => {
       expect(isAuthorized({ email: "teste@teste.com" })).toBeTruthy();
     });
 
@@ -57,20 +57,25 @@ describe(".userAuthorization()", () => {
   describe("when user list has values", () => {
     const isAuthorized = userAuthorization(["teste@gmail.com"]);
 
-    it("should return true when email is listed", () => {
+    it("should return true when the correct email is listed", () => {
       expect(isAuthorized({ email: "teste@gmail.com" })).toBeTruthy();
     });
 
-    it("should return false when email isn't listed", () => {
+    it("should return false when email isn't in the list", () => {
       expect(isAuthorized({ email: "teste2@gmail.com" })).toBeFalsy();
     });
 
-    it("should return false when email is empty string", () => {
+    it("should return false when email is an empty string", () => {
       expect(isAuthorized({ email: "" })).toBeFalsy();
     });
 
     it("should return false when email is undefined", () => {
       expect(isAuthorized({ email: undefined })).toBeFalsy();
     });
+
+    it("should return false when email is poorly formatted", () => {
+      expect(isAuthorized({ email: " teste@gmail.com " })).toBeFalsy();
+    });
+
   });
 });
