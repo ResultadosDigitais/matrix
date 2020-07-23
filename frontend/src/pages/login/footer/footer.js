565c4e9bcae3caca88015b0e7bc756fb412eb780
Phiter Fernandes <phiterfernandes@gmail.com>
2020-07-22 23:50:11 -0300

    style: fixes footer jsx identation

--- frontend/src/pages/login/footer/footer.js
+++ frontend/src/pages/login/footer/footer.js
@@ -4,9 +4,9 @@ import { Trans } from "react-i18next";
 import styles from "./footer.module.css";
 
 export const Footer = () => (
-    <div
-      className={`row p-5 justify-content-center align-items-center ${styles.bottomdiv}`}
-    >
+  <div
+    className={`row p-5 justify-content-center align-items-center ${styles.bottomdiv}`}
+  >
     <Trans
       i18nKey="auth:footer"
       components={{ heart: <span className={styles.heart} /> }}
