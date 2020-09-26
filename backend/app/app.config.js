be13e84f03673f2db1c1aad5d583ab3383e3bd1f
Silvana Lima <silvana.vieira@hotmail.com>
2020-09-26 20:25:25 -0300

    refactor:[APPCONFIG] Refactoring magic number for variable

--- backend/app/app.config.js
+++ backend/app/app.config.js
@@ -2,7 +2,7 @@ import environment from "./helpers/environment";
 
 export function getRoomsSource() {
   const roomsSource = process.env.ROOMS_SOURCE;
-  return roomsSource
+  return roomsSource;
 }
 
 export function shouldEnforceSSL() {
@@ -16,16 +16,15 @@ export function getEnvironment() {
 }
 
 export function getSessionConfig() {
-  const cookieSessionKey =
-    process.env.COOKIE_SESSION_SECRET || "matrix-session";
-  const maxAge =
-    environment.parseVariable(process.env.COOKIE_SESSION_MAX_AGE) ||
-    30 * 24 * 60 * 60 * 1000; // 30 days
+  const cookieSessionKey = process.env.COOKIE_SESSION_SECRET || "matrix-session";
+  const thirtyDays = 30 * 24 * 60 * 60 * 1000;
+  const maxAge = environment.parseVariable(process.env.COOKIE_SESSION_MAX_AGE)
+    || thirtyDays;
 
   return {
     name: "matrix-session",
     keys: [cookieSessionKey],
-    maxAge
+    maxAge,
   };
 }
 
@@ -33,35 +32,32 @@ export function getAuthConfig() {
   const defaultAuthStrategy = "google";
   const clientID = process.env.GOOGLE_CLIENT_ID || "XXXX";
   const clientSecret = process.env.GOOGLE_SECRET || "XXXX";
-  const callbackURL =
-    process.env.GOOGLE_CALLBACK_URL ||
-    "http://localhost:8080/auth/google/callback";
+  const callbackURL = process.env.GOOGLE_CALLBACK_URL
+    || "http://localhost:8080/auth/google/callback";
 
   return {
     authStrategy: defaultAuthStrategy,
     clientID,
     clientSecret,
-    callbackURL
+    callbackURL,
   };
 }
 
 export function getAllowedDomains() {
-  const allowedDomains =
-    environment.parseVariable(process.env.WHITELIST_DOMAINS) || [];
+  const allowedDomains = environment.parseVariable(process.env.WHITELIST_DOMAINS) || [];
 
   return allowedDomains;
 }
 
 export function getAllowedUsers() {
-  const allowedUsers =
-    environment.parseVariable(process.env.WHITELIST_USERS) || [];
+  const allowedUsers = environment.parseVariable(process.env.WHITELIST_USERS) || [];
 
   return allowedUsers;
 }
 
 export function getServerConfig() {
-  const host = "0.0.0.0"
-  const port = process.env.PORT || 8080
+  const host = "0.0.0.0";
+  const port = process.env.PORT || 8080;
 
-  return { host, port }
+  return { host, port };
 }
