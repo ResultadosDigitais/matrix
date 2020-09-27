7224fa363a8a75f725ff5ea1c8518576195f6d83
Silvana Lima <silvana.vieira@hotmail.com>
2020-09-27 17:43:20 -0300

    fix: [logo] add alt attribute for logo, if the image cannot be displayed.

--- frontend/src/pages/login/logo/logo.js
+++ frontend/src/pages/login/logo/logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Logo = () => (
   <div className="row justify-content-center align-items-center">
-    <img src="/images/logo.svg" />
+    <img src="/images/logo.svg" alt="Matrix's logo" />
   </div>
 );
