a0d16fa2a9f2adb3aeb9acbad958f8b86dc03871
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-12 22:51:11 -0300

    feat: add methods to update and retrieve data from localstorage

--- frontend/src/morpheus/store/storage.js
+++ frontend/src/morpheus/store/storage.js
@@ -1,5 +1,6 @@
 const STORAGE_KEY_MEETING_SETTINGS = "meetingSettings";
 const STORAGE_KEY_THEME = "color-scheme";
+const STORAGE_KEY_NOTIFICATION = "notification-disabled";
 
 const storage = {
   getMeetingSettings: defaultValue => {
@@ -25,7 +26,19 @@ const storage = {
   getTheme: defaultTheme =>
     localStorage.getItem(STORAGE_KEY_THEME) || defaultTheme,
 
-  setTheme: theme => localStorage.setItem(STORAGE_KEY_THEME, theme)
+  setTheme: theme => localStorage.setItem(STORAGE_KEY_THEME, theme),
+
+  setNotificationDisabled: value => localStorage.setItem(STORAGE_KEY_NOTIFICATION, value),
+
+  isNotificationDisabled: defaultValue => {
+    const value = localStorage.getItem(STORAGE_KEY_NOTIFICATION);
+
+    if (value) {
+      return JSON.parse(value);
+    }
+
+    return defaultValue;
+  },
 };
 
 export default storage;
