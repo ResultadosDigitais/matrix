a6bb8294150237580a27becbf1e249b4d9fcbfc6
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2021-10-04 09:15:48 -0300

    feat: change healthcheck to solve a unauthorized rooms list

--- backend/app/app.healthcheck.js
+++ backend/app/app.healthcheck.js
@@ -1,13 +1,9 @@
 import { createTerminus } from "@godaddy/terminus";
-import fetchRooms from "./services/rooms";
-import { getRoomsSource } from "./app.config";
 
 function onHealthCheck() {
   // checks if the system is healthy,
   // we can add the checks here as needed
-  return Promise.all(
-    [fetchRooms(getRoomsSource())],
-  );
+  return Promise.all(['OK']);
 }
 
 module.exports = (server) => {
